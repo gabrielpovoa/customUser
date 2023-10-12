972464bbeaaf4acd7dda9cc06e0768f6988e61b6
João <povoajoao6@gmail.com>
2023-10-12 15:12:00 -0300

    FEATURE: Creating a page where you can build your own custom character

---
+++ src/app/components/CustomClient.tsx
@@ -0,0 +1,32 @@
+interface User {
+  name: string,
+  description: string,
+  avatar: string,
+  onClick: () => void;
+}
+
+export const CustomClient = ({ name, description, avatar, onClick }: User) => {
+  return (
+    <div>
+      <div className="flex flex-col items-center gap-2 bg-white p-3 rounded-sm mb-5">
+        <img
+          src={avatar}
+          alt={description}
+          className="max-w-xl rounded mt-3"
+          style={{height:'20rem'}}
+        />
+        <div className="max-w-xl ">
+          <h2 className="text-slate-500 font-semibold mb-2 text-2xl">{name}</h2>
+          <p className="text-black">{description}</p>
+        </div>
+      </div>
+      <button
+        onClick={onClick}
+        className="text-white p-3 rounded-sm"
+        style={{backgroundColor:"#192A51"}}
+      >
+        Change Picture
+      </button>
+    </div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { useState } from "react";
+import { CustomClient } from "./components/CustomClient"
+
+let identifier: number;
+const generateId = () => {
+  let time = new Date().getMilliseconds();
+  identifier = time;
+}
+generateId()
+
+export default function Home() {
+  const [avatar, setAvatar] = useState("https://staticg.sportskeeda.com/editor/2022/08/53e15-16596004347246.png?w=840");
+  const [name, setName] = useState("Naruto Uzumaki");
+  const [description, setDescription] = useState("Naruto Uzumaki is the titular protagonist of the manga Naruto, created by Masashi Kishimoto. As the series progresses, he is a young ninja from the fictional village of Konohagakure");
+
+  const updateUserData = () => {
+    let newAvatar: any = prompt();
+    let newName: any = prompt();
+    let newDescription: any = prompt();
+
+    setAvatar(newAvatar);
+    setName(newName);
+    setDescription(newDescription);
+  }
+
+  return <>
+    <h1 className="text-3xl font-bold text-center mb-3">
+      Custom User
+    </h1>
+    <div className="max-w-2xl m-auto">
+      <CustomClient
+        avatar={avatar}
+        name={name}
+        description={description}
+        onClick={updateUserData}
+        key={identifier}
+      />
+    </div>
+  </>
+}
