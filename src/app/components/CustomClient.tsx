955a3335ae61323c790850a0804d84a8ce6b67f4
João <povoajoao6@gmail.com>
2023-10-12 21:30:45 -0300

    change some styles

--- src/app/components/CustomClient.tsx
+++ src/app/components/CustomClient.tsx
@@ -7,13 +7,13 @@ interface User {
 
 export const CustomClient = ({ name, description, avatar, onClick }: User) => {
   return (
-    <div>
+    <div className="flex justify-start items-center flex-col">
       <div className="flex flex-col items-center gap-2 bg-white p-3 rounded-sm mb-5">
         <img
           src={avatar}
           alt={description}
           className="max-w-xl rounded mt-3"
-          style={{height:'20rem'}}
+          style={{ height: '20rem' }}
         />
         <div className="max-w-xl ">
           <h2 className="text-slate-500 font-semibold mb-2 text-2xl">{name}</h2>
@@ -23,8 +23,7 @@ export const CustomClient = ({ name, description, avatar, onClick }: User) => {
       <button
         onClick={onClick}
         className="text-white p-3 rounded-sm"
-        style={{backgroundColor:"#192A51"}}
-      >
+        style={{ backgroundColor: "#192A51" }}>
         Change Picture
       </button>
     </div>
